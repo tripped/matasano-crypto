1fd56dd739a62a6fe8d85f34c1d55bccf13e95c7
Trip Volpe <trip.volpe@gmail.com>
2016-04-26 20:22:03 -0700

    Add xor and failing unit test

--- src/main.rs
+++ src/main.rs
@@ -164,6 +164,17 @@ fn hex_to_base64_works() {
     assert_eq!(hex_to_base64(hex), expected);
 }
 
+fn xor(a: &str, b: &str) -> String {
+    String::new()
+}
+
+#[test]
+fn xor_works() {
+    let a = "1c0111001f010100061a024b53535009181c";
+    let b = "686974207468652062756c6c277320657965";
+    assert_eq!(xor(a, b), "746865206b696420646f6e277420706c6179");
+}
+
 fn main() {
     println!("Hello, world!");
 }
